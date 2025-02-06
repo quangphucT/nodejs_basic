9fcd88d48426535b3d5d3b4225d133772f757aef
conbocuoila <phucp9698@gmail.com>
2025-02-06 20:13:23 +0700

    update

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+const app = express(); // tạo app có thể sự dụng được  all tính năng của express, gọi hàm express()
+const port = 3000;
+
+configViewEngine(app);
+app.get("/", (req, res) => {
+  res.render('index.ejs')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
